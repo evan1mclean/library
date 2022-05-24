6e62ee6658fd7612b1dd0a34ade203c69cc86e00
Evan McLean <evanhughmclean@gmail.com>
2022-05-24 16:25:13 -0400

    Adds modal and form content

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
 let myLibrary = [];
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
+const modalButton = document.querySelector("#bookButton");
+
+modalButton.addEventListener("click", toggleModal);
+window.addEventListener("mousedown", windowOnClick);
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -7,5 +13,20 @@ function Book(title, author, pages, read) {
     this.read = read;
     this.info = function() {
         return `${title} by ${author}, ${pages} pages, have ${read}`;
+    }
+}
+
+function addBookToLibrary() {
+
+}
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+    modalContent.classList.toggle("show-modal");
+}
+
+function windowOnClick(e) {
+    if (e.target === modal) {
+        toggleModal();
     }
 }
